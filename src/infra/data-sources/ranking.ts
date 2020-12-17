f615400ecb0280ecb78e6993efb16d9dba869b1e
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-12-17 11:01:52 -0300

    add fake rankings data source

---
+++ src/infra/data-sources/ranking.ts
@@ -0,0 +1,137 @@
+export const ranking = [{
+  user: {
+    country: 'Brazil',
+    name: 'Rodrigo Manguinho'
+  },
+  heroes: [{
+    name: 'Fee',
+    level: 30
+  }, {
+    name: 'Connie',
+    level: 23
+  }, {
+    name: 'Mabyn',
+    level: 27
+  }],
+  date: 1578625200000,
+  score: 455
+}, {
+  user: {
+    country: 'Brazil',
+    name: 'Rodrigo Branas'
+  },
+  heroes: [{
+    name: 'Koizuul',
+    level: 26
+  }, {
+    name: 'Connie',
+    level: 23
+  }, {
+    name: 'Caldera',
+    level: 27
+  }],
+  date: 1507345200000,
+  score: 570
+}, {
+  user: {
+    country: 'Brazil',
+    name: 'Erick Wendel'
+  },
+  heroes: [{
+    name: 'Azura',
+    level: 23
+  }, {
+    name: 'Hogan',
+    level: 23
+  }, {
+    name: 'Mabyn',
+    level: 27
+  }],
+  date: 1481508000000,
+  score: 300
+}, {
+  user: {
+    country: 'Brazil',
+    name: 'Otavio Lemos'
+  },
+  heroes: [{
+    name: 'Raida',
+    level: 20
+  }, {
+    name: 'Koizuul',
+    level: 13
+  }, {
+    name: 'Caldera',
+    level: 17
+  }],
+  date: 1424574000000,
+  score: 290
+}, {
+  user: {
+    country: 'Portugal',
+    name: 'Caio Zulo'
+  },
+  heroes: [{
+    name: 'Bolton',
+    level: 28
+  }, {
+    name: 'Efrigid',
+    level: 29
+  }, {
+    name: 'Yan',
+    level: 27
+  }],
+  date: 1579662000000,
+  score: 600
+}, {
+  user: {
+    country: 'USA',
+    name: 'Robert Martin'
+  },
+  heroes: [{
+    name: 'Koizuul',
+    level: 30
+  }, {
+    name: 'Connie',
+    level: 30
+  }, {
+    name: 'Raida',
+    level: 27
+  }],
+  date: 1591066800000,
+  score: 610
+}, {
+  user: {
+    country: 'England',
+    name: 'Eric Evans'
+  },
+  heroes: [{
+    name: 'Sethos',
+    level: 20
+  }, {
+    name: 'Helios',
+    level: 23
+  }, {
+    name: 'Leif',
+    level: 17
+  }],
+  date: 1553914800000,
+  score: 200
+}, {
+  user: {
+    country: 'England',
+    name: 'Martin Fowler'
+  },
+  heroes: [{
+    name: 'Shamiko',
+    level: 30
+  }, {
+    name: 'Koizuul',
+    level: 30
+  }, {
+    name: 'Raida',
+    level: 30
+  }],
+  date: 1548036000000,
+  score: 900
+}]
